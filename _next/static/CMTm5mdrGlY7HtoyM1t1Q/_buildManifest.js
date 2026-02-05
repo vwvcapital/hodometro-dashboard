db1e6c95550fdd55af2cd7bff8eae81886e52412
VWV Capital <vwvcapital@gmail.com>
2026-02-05 13:51:21 -0300

    Updates

---
+++ _next/static/CMTm5mdrGlY7HtoyM1t1Q/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hodometro-dashboard//_next/:path+",
+        "destination": "/hodometro-dashboard/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
